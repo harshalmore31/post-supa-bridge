84090a120a8a6ca707d248481a3d8b3575468f3f
Rajmali-22 <22.raj.mali@gmail.com>
2025-03-28 02:33:25 +0530

    ai_marks_removed

--- supa/main.py
+++ supa/main.py
@@ -1,13 +1,8 @@
 import database_operations as db
 from api import app, socketio
 
-# --- Main Application Startup ---
+
 if __name__ == '__main__':
-    # Perform initial data load check
     db.initial_data_load()
-    
-    # Start database listener thread
     db.start_db_listener(socketio)
-    
-    # Start Flask-SocketIO server
     socketio.run(app, host='0.0.0.0', port=5000)
